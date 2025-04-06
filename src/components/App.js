01a361d49d1b88cfedd59e30706e874150b60a2c
ashish <ashishmasih92g@gmail.com>
2025-04-06 15:26:13 +0530

    react-assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [count,setCount] = useState(0)
+  console.log(count)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p style={{fontWeight:"bolder"}}> Button clicked {count} times</p>
+      <button onClick={()=>{
+        setCount(count+1)
+      }}>Click</button>
     </div>
   )
 }
